905cbddfa0fe023415c298408776afcaf6c8511e
warizz yutanan <warizz.yutanan@gmail.com>
2017-06-19 16:49:01 +0700

    Style trips/trip

--- src/trip.js
+++ src/trip.js
@@ -1,14 +1,70 @@
 import React, { Component } from 'react';
+import moment from 'moment';
+import styled from 'styled-components';
+
+const Container = styled.div.attrs({
+  tabIndex: 0
+})`
+  padding: 10px;
+  display: flex;
+  align-items: center;
+`;
+
+const AvatarContainer = styled.div`
+  width: 5em;
+  height: 5em;
+`;
+
+const Image = styled.img`
+  width: 5em;
+  height: 5em;
+  border-radius: 100%;
+`;
+
+const Info = styled.div`
+  height: 2em;
+  display: flex;
+  align-items: center;
+  margin: 0 0 0 1em;
+`;
+
+const InfoText = styled.div`
+  margin: 0 0 0 1em;
+`;
+
+const JoinButton = styled.button`
+  width: 5em;
+  height: 5em;
+  background-color: transparent;
+  margin: 0 0 0 1em;
+  border: none;
+`;
 
 class Trip extends Component {
   render() {
-    const { destination_name, id, travel_time } = this.props.trip;
+    const { destination_name, travel_time } = this.props.trip;
+
     return (
-      <div>
-        <div>{id}</div>
-        <div>{destination_name}</div>
-        <div>{travel_time}</div>
-      </div>
+      <Container className="trip-component">
+        <AvatarContainer>
+          <Image src="https://avatars2.githubusercontent.com/u/25485275?v=3&u=4f2f71b977544a61c63121d4bea843e4f2d10b2a&s=400" />
+        </AvatarContainer>
+        <div>
+          <Info height="5em">
+            <i className="material-icons">directions_car</i>
+            <InfoText>{destination_name}</InfoText>
+          </Info>
+          <Info>
+            <i className="material-icons">access_time</i>
+            <InfoText>
+              {moment(travel_time).format('DD MMM YYYY-HH:mm')}
+            </InfoText>
+          </Info>
+        </div>
+        <JoinButton>
+          <i className="material-icons">plus_one</i>
+        </JoinButton>
+      </Container>
     );
   }
 }
--- src/trips.js
+++ src/trips.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 import Trip from './trip';
+
+const List = styled.ul`
+  list-style-type: none;
+`;
 
 class Trips extends Component {
   render() {
     return (
       <div>
-        {this.props.trips.map(trip => <Trip key={trip.id} trip={trip} />)}
+        <List>
+          {this.props.trips.map(trip =>
+            <li key={trip.id}>
+              <Trip trip={trip} />
+            </li>
+          )}
+        </List>
       </div>
     );
   }
